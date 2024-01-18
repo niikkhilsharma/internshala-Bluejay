ee3867273ef72c7bea3152b40d6ed4da68f2e656
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-01-18 12:06:02 +0530

    Added backend api to get the data on the basis of the filters and also created the frontend applicatoin

--- src/app/read/page.jsx
+++ src/app/read/page.jsx
@@ -23,29 +23,43 @@ export default function Page() {
 		<div className="container mx-auto">
 			<div className="my-10">
 				<div className="text-center w-full text-xl font-semibold underline">
-					Categorize Employees with the help of these filters.
+					Categorize Employees with the help of these filters
 				</div>
 				<div className="my-10 flex justify-evenly items-center">
 					<span
-						className="inline-flex items-center rounded-md bg-indigo-50 px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer hover:bg-indigo-100"
+						className={`inline-flex items-center rounded-md ${
+							filter === 'sevenDays'
+								? 'bg-yellow-600 text-white shadow-xl'
+								: 'bg-indigo-50 hover:bg-indigo-100'
+						} px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer`}
 						onClick={() => setFilter('sevenDays')}
 					>
 						Has worked for 7 consecutive days.
 					</span>
 					<span
-						className="inline-flex items-center rounded-md bg-indigo-50 px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer hover:bg-indigo-100"
+						className={`inline-flex items-center rounded-md ${
+							filter === 'lessThan10GreaterThan1'
+								? 'bg-yellow-600 text-white shadow-xl'
+								: 'bg-indigo-50 hover:bg-indigo-100'
+						} px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer `}
 						onClick={() => setFilter('lessThan10GreaterThan1')}
 					>
 						have less than 10 hours of time between shifts but greater than 1 hour
 					</span>
 					<span
-						className="inline-flex items-center rounded-md bg-indigo-50 px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer hover:bg-indigo-100"
+						className={`inline-flex items-center rounded-md ${
+							filter === 'moreThan14'
+								? 'bg-yellow-600 text-white shadow-xl'
+								: 'bg-indigo-50 hover:bg-indigo-100'
+						} px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer `}
 						onClick={() => setFilter('moreThan14')}
 					>
 						has worked for more than 14 hours in a single shift
 					</span>
 					<span
-						className="inline-flex items-center rounded-md bg-indigo-50 px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer hover:bg-indigo-100"
+						className={`inline-flex items-center rounded-md ${
+							filter === 'none' ? 'bg-yellow-600 text-white shadow-xl' : 'bg-indigo-50'
+						} px-4 py-2 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 hover:cursor-pointer hover:bg-indigo-100`}
 						onClick={() => setFilter('none')}
 					>
 						Clear Filters
@@ -56,9 +70,12 @@ export default function Page() {
 				<div className="text-center text-xl font-semibold">Loading Data...</div>
 			) : (
 				<div>
-					<table className="table-auto w-full">
+					<table className="table-auto w-full text-center">
 						<thead>
 							<tr>
+								<th className="px-4 py-2 border">
+									<span className="text-xl font-semibold">S.No</span>
+								</th>
 								{Object.keys(data[0])
 									.filter(key => keysToShow.includes(key))
 									.map((item, index) => (
@@ -71,6 +88,7 @@ export default function Page() {
 						<tbody>
 							{data.map((item, index) => (
 								<tr key={index}>
+									<td className="border px-4 py-2">{index + 1}</td>
 									{Object.keys(item)
 										.filter(key => keysToShow.includes(key))
 										.map((key, index) => (
